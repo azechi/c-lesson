6ebc875801f5261b0a30034aaeb35d2160b17969
azechi <azechi.n@gmail.com>
2019-04-27 16:37:18 +0900

    01_int_parser

--- sources/forth_modoki/01_int_parser/int_parser.c
+++ sources/forth_modoki/01_int_parser/int_parser.c
@@ -10,7 +10,29 @@ int main() {
     int answer3 = 0;
 
     // write something here.
+    int* ans[] = {&answer1, &answer2, &answer3};
+    int p = 0;
+ 
+    const char* i = input;
+    int acm = 0;
+    
+    while(*i) {
+        if(*i == ' '){
+            *ans[p++] = acm;
+            acm = 0;
 
+            while(*++i == ' ')
+                ;
+            
+            continue;
+        }
+
+        acm = acm * 10 + *i - '0';
+        
+        i++;
+    }
+
+    *ans[p] = acm;
 
     // verity result.
     assert(answer1 == 123);

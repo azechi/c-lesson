b3a792ba54bc0c48a9d3a9c4e314d31fe0c4760d
azechi <azechi.n@gmail.com>
2019-05-01 08:41:50 +0900

    Add unit tests

---
+++ sources/forth_modoki/04_stack/main.c
@@ -0,0 +1,29 @@
+#include "stack.h"
+#include <assert.h>
+
+
+static void test_stack_pop() {
+    assert(0);
+}
+
+static void test_stack_push() {
+    assert(0);
+}
+
+static void test_stack_push_pop() {
+    assert(0);
+}
+
+static void test_stack_push_push_pop_pop() {
+    assert(0);
+}
+
+int main() {
+
+    test_stack_pop();
+    test_stack_push();
+    test_stack_push_pop();
+    test_stack_push_push_pop_pop();
+
+    return 1;
+}

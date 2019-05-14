4cc89cd8111e72844aab313a8853ad3aee1d4f41
azechi <azechi.n@gmail.com>
2019-05-14 12:02:11 +0900

    Update parse nagative number

--- sources/forth_modoki/interpreter/parser.c
+++ sources/forth_modoki/interpreter/parser.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <assert.h>
 
+#include "util.h"
 #include "parser.h"
 
 #define NAME_SIZE 256
@@ -26,7 +27,7 @@ static int is_digit(char ch) {
 }
 
 static int is_alpha(char ch) {
-    return ('a' <= ch && ch <= 'z') 
+    return ('a' <= ch && ch <= 'z')
         || ('A' <= ch && ch <= 'Z');
 }
 
@@ -34,6 +35,113 @@ static int is_alpha_digit(char ch) {
     return is_digit(ch) || is_alpha(ch);
 }
 
+
+static int token_equals(const Token *t1, const Token *t2) {
+    if(!(t1 && t2
+                && t1->ltype == t2->ltype)){
+        return 0;
+    }
+
+    switch(t1->ltype) {
+        case LEX_NUMBER:
+            return (t1->u.number == t2->u.number);
+        case LEX_LITERAL_NAME:
+        case LEX_EXECUTABLE_NAME:
+            return streq(t1->u.name, t2->u.name);
+        case LEX_OPEN_CURLY:
+        case LEX_CLOSE_CURLY:
+        case LEX_SPACE:
+            return (t1->u.onechar == t2->u.onechar);
+        case LEX_END_OF_FILE:
+            return 1;
+        case LEX_UNKNOWN:
+            return 0;
+    }
+}
+
+static void verify_parse_one(const char *input, const Token *expect, int expect_prev_ch) {
+    cl_getc_set_src(input);
+
+    Token actual = {0};
+    int actual_prev_ch = parse_one(EOF, &actual);
+    int eq = token_equals(expect, &actual);
+    assert(eq && actual_prev_ch);
+}
+
+static void verify_parse_one_number(const char *input, int expect_number, int expect_prev_ch) {
+    Token expect = {LEX_NUMBER, .u.number = expect_number};
+    verify_parse_one(input, &expect, expect_prev_ch);
+}
+
+static void test_parse_one_number() {
+    verify_parse_one_number("123", 123, EOF);
+    verify_parse_one_number("-123", -123, EOF);
+    verify_parse_one_number("1{", 1, '{');
+}
+
+static int parse_one_number(int prev_ch, int ch, Token *out_token) {
+    int is_positive = prev_ch != '-';
+    int acm = (is_positive) ? prev_ch - '0' : 0;
+
+    while (is_digit(ch)) {
+        acm = acm * 10 + ch - '0';
+        ch = cl_getc();
+    }
+
+    out_token->ltype = LEX_NUMBER;
+    out_token->u.number = (is_positive) ? acm : -acm;
+
+    return ch;
+}
+
+
+static void verify_parse_one_executable_name(const char *input, const char *expect_name, int expect_prev_ch) {
+    Token expect = {LEX_EXECUTABLE_NAME, .u.name = (char*)expect_name};
+    verify_parse_one(input, &expect, expect_prev_ch);
+}
+
+static void verify_parse_one_literal_name(const char *input, const char *expect_name, int expect_prev_ch) {
+    Token expect = {LEX_LITERAL_NAME, .u.name = (char*)expect_name};
+    verify_parse_one(input, &expect, expect_prev_ch);
+}
+
+static void test_parse_one_executable_name() {
+    verify_parse_one_executable_name("ab", "ab", EOF);
+    verify_parse_one_executable_name("a123", "a123", EOF);
+    verify_parse_one_executable_name("a123{", "a123", '{');
+}
+
+static void test_parse_one_literal_name() {
+    verify_parse_one_literal_name("/ab", "ab", EOF);
+    verify_parse_one_literal_name("/a123", "a123", EOF);
+    verify_parse_one_literal_name("/a123{", "a123", '{');
+}
+
+static int parse_one_name(int prev_ch, int ch, Token *out_token) {
+    int is_exec_name = prev_ch != '/';
+
+    int i = 0;
+    char buf[NAME_SIZE];
+
+    if (is_exec_name) {
+        buf[i++] = prev_ch;
+    } else {
+        buf[i++] = ch;
+        ch = cl_getc();
+    }
+
+    while (is_alpha_digit(ch)) {
+        buf[i++] = ch;
+        ch = cl_getc();
+    }
+    buf[i] = '\0';
+
+    char* name = malloc_copy_str(i + 1, buf);
+    out_token->ltype = (is_exec_name) ? LEX_EXECUTABLE_NAME : LEX_LITERAL_NAME;
+    out_token->u.name = name;
+
+    return ch;
+}
 
 int parse_one(int prev_ch, Token *out_token) {
     int ch;
@@ -46,17 +154,14 @@ int parse_one(int prev_ch, Token *out_token) {
 
     if (prev_ch == EOF) {
         out_token->ltype = LEX_END_OF_FILE;
-    } else if (is_digit(prev_ch)) {
-        int acm = prev_ch - '0';
-
-        while (is_digit(ch)) {
-            acm = acm * 10 + ch - '0';
-            ch = cl_getc();
-        }
-
-        out_token->ltype = LEX_NUMBER;
-        out_token->u.number = acm;
-    } else if (prev_ch == ' ') {
+    } else if (is_digit(prev_ch)
+                || (prev_ch == '-' && is_digit(ch))) {
+        ch = parse_one_number(prev_ch, ch, out_token);
+    } else if (is_alpha(prev_ch)
+            || (prev_ch == '/' && is_alpha(ch)))
+    {
+        ch = parse_one_name(prev_ch, ch, out_token);
+    }  else if (prev_ch == ' ') {
 
         while (ch == ' '){
             ch = cl_getc();
@@ -64,37 +169,6 @@ int parse_one(int prev_ch, Token *out_token) {
 
         out_token->ltype = LEX_SPACE;
         out_token->u.onechar  = ' ';
-    } else if (is_alpha(prev_ch)) {
-        char buf[NAME_SIZE];
-        int i = 0;
-
-        buf[i++] = prev_ch;
-
-        while (is_alpha_digit(ch)) {
-            buf[i++] = ch;
-            ch = cl_getc();
-        }
-        buf[i] = '\0';
-
-        char* name = malloc_copy_str(i + 1, buf);
-        out_token->ltype = LEX_EXECUTABLE_NAME;
-        out_token->u.name = name;
-    } else if (prev_ch == '/' && is_alpha(ch)) {
-        char buf[NAME_SIZE];
-        int i = 0;
-
-        buf[i++] = ch;
-        ch = cl_getc();
-
-        while (is_alpha_digit(ch)) {
-            buf[i++] = ch;
-            ch = cl_getc();
-        }
-        buf[i] = '\0';
-
-        char* name = malloc_copy_str(i + 1, buf);
-        out_token->ltype = LEX_LITERAL_NAME;
-        out_token->u.name = name;
     } else if (prev_ch == '{') {
         out_token->ltype = LEX_OPEN_CURLY;
         out_token->u.onechar = '{';
@@ -162,22 +236,6 @@ static void test_parse_one_empty_should_return_END_OF_FILE() {
     assert(token.ltype == expect);
 }
 
-static void test_parse_one_number() {
-    char *input = "123";
-    int expect = 123;
-
-    Token token = {LEX_UNKNOWN, {0}};
-    int ch;
-
-    cl_getc_set_src(input);
-
-    ch = parse_one(EOF, &token);
-
-    assert(ch == EOF);
-    assert(token.ltype == LEX_NUMBER);
-    assert(expect == token.u.number);
-}
-
 static void test_malloc_copy_str_empty() {
     int input_size = 1;
     char* input_str = "";
@@ -198,39 +256,6 @@ static void test_malloc_copy_str() {
     assert(strcmp(actual, input_str) == 0);
 }
 
-static void test_parse_one_executable_name() {
-    char* input = "add";
-    int expect_type = LEX_EXECUTABLE_NAME;
-    char* expect_name = "add";
-
-    Token actual = {LEX_UNKNOWN, {0}};
-    int ch;
-
-    cl_getc_set_src(input);
-
-    ch = parse_one(EOF, &actual);
-
-    assert(EOF == ch);
-    assert(expect_type == actual.ltype);
-    assert(strcmp(expect_name, actual.u.name) == 0);
-}
-
-static void test_parse_one_literal_name() {
-    char* input = "/add";
-    int expect_type = LEX_LITERAL_NAME;
-    char* expect_name = "add";
-
-    Token actual = {LEX_UNKNOWN, {0}};
-    int ch;
-
-    cl_getc_set_src(input);
-
-    ch = parse_one(EOF, &actual);
-
-    assert(EOF == ch);
-    assert(expect_type == actual.ltype);
-    assert(strcmp(expect_name, actual.u.name) == 0);
-}
 
 static void test_parse_one_open_curly() {
     char* input = "{";
@@ -277,6 +302,7 @@ void parser_test_all() {
     test_parse_one_literal_name();
     test_parse_one_open_curly();
     test_parse_one_close_curly();
+
 }
 
 #if 0

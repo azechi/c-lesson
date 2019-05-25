d4817713427835254c65c220197206ca994e1877
azechi <azechi.n@gmail.com>
2019-05-25 10:30:17 +0900

    Complete putchar_mem.s

--- sources/arm_asm/02_arm_assembly/2_5_print_mem/putchar_mem.s
+++ sources/arm_asm/02_arm_assembly/2_5_print_mem/putchar_mem.s
@@ -1,30 +1,42 @@
 .globl _start
 _start:
 
-  ldr r0,=msg1
+  ldr r13, =0x08000000
+  ldr r0, =msg1
   bl print
-  ldr r0,=msg2
+  ldr r0, =msg2
   bl print
 end:
   b end
 
+/*
+  putchar:
+    arg r0: target charactor which is printed.
+    used internal register: r1.
+*/
 putchar:
-  // TODO: fix here!
-  ldr r1,=0x101f1000
-  ldr r0, [r1]
+  stmdb r13!, {r1, r14}
+  ldr r1, =0x101f1000
+  str r0, [r1]
+  ldmia r13!, {r1, r14}
   mov r15, r14
 
-
+/*
+  print:
+    arg r0: Address of target string, must end by \0.
+    used internal register: r1
+*/
 print:
-  // TODO: Fix this function too.
-  ldrb r3,[r0]
-_loop:  
-  
-
-  add r0, r0, #1
-  ldrb r3,[r0]
-  cmp r3,#0
+  stmdb r13!, {r1, r14}
+  mov r1, r0
+  ldrb r0, [r1] // load charactor
+_loop:
+  bl putchar
+  add r1, r1, #1
+  ldrb r0, [r1]
+  cmp r0, #0
   bne _loop
+  ldmia r13!, {r1, r14}
   mov r15, r14
 
 msg1: .asciz "First text.\n"
